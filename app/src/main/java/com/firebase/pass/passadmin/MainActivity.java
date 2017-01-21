8681cb9c3a02d0735735f4ee058e0752a28a4b07
Dwijraj <funkymaina@gmail.com>
2017-01-22 01:36:48 +0530

    Authenticating a guard and Changing Gate password

--- app/src/main/java/com/firebase/pass/passadmin/MainActivity.java
+++ app/src/main/java/com/firebase/pass/passadmin/MainActivity.java
@@ -1,27 +1,158 @@
 package com.firebase.pass.passadmin;
 
+import android.app.Dialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionBarOverlayLayout;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button AUTHENTICATE_GAURD;
+    private Button VIEW_PASS;
+    private EditText PASS_NUMBER;
+    private Button CHANGE_GATE_PASSWORD;
+    private Dialog dialog;
+    private DatabaseReference Gaurds;
     private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth=FirebaseAuth.getInstance();
+        Gaurds= FirebaseDatabase.getInstance().getReference();
+        dialog= new Dialog(this);
+        dialog.setCanceledOnTouchOutside(false);
+        AUTHENTICATE_GAURD=(Button)findViewById(R.id.AUTHENTICATE_USER_ID);
+        VIEW_PASS=(Button)findViewById(R.id.VIEW_PASS_ID);
+        CHANGE_GATE_PASSWORD=(Button)findViewById(R.id.CHANGE_GATE_PASSWORD_ID);
+        PASS_NUMBER=(EditText)findViewById(R.id.PASS_NUMBER_ID);
 
-     /*   if(mAuth.getCurrentUser()==null)
-        {
-            Intent SIGN_IN=new Intent(MainActivity.this,sign_In_Activity.class);
-            finish();
-            startActivity(SIGN_IN);
-        } */
+
+
+
+
+
+
+        CHANGE_GATE_PASSWORD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.gaurd_gate);
+                dialog.show();
+
+                final EditText NAME=(EditText) dialog.findViewById(R.id.GateANameEdit);
+                final EditText PHONE=(EditText) dialog.findViewById(R.id.GateBPhoneEdit);
+                Button SUBMIT=(Button) dialog.findViewById(R.id.Submit_button);
+
+                SUBMIT.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if(!TextUtils.isEmpty(NAME.getText().toString().trim()))
+                        {
+                            Gaurds.child("Gate").child("GateA").setValue(NAME.getText().toString().trim());
+
+                        }
+                        if(!TextUtils.isEmpty(PHONE.getText().toString().trim()))
+                        {
+                            Gaurds.child("Gate").child("GateB").setValue(PHONE.getText().toString().trim());
+
+                        }
+
+                    }
+                });
+
+
+
+            }
+        });
+        AUTHENTICATE_GAURD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.gaurd_gate);
+                dialog.show();
+
+                TextView Name =(TextView) dialog.findViewById(R.id.GateANameText);
+                TextView Password=(TextView) dialog.findViewById(R.id.GateBPhoneText);
+                final EditText NAME=(EditText) dialog.findViewById(R.id.GateANameEdit);
+                final EditText PHONE=(EditText) dialog.findViewById(R.id.GateBPhoneEdit);
+                Button SUBMIT=(Button) dialog.findViewById(R.id.Submit_button);
+                NAME.setHint("Name of the Gaurd");
+                PHONE.setHint("Mobile number of the Gaurd");
+                Password.setText("Password");
+                Name.setText("Name");
+
+                SUBMIT.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        final String Name =NAME.getText().toString().trim();
+                        final String Phone=PHONE.getText().toString().trim();
+
+                        if(!(TextUtils.isEmpty(Name)|| TextUtils.isEmpty(Phone) || Phone.length()!=10))
+                        {
+                            mAuth.createUserWithEmailAndPassword(Phone+"@"+Phone+".com",Phone).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+
+                                    mAuth.signInWithEmailAndPassword(Phone+"@"+Phone+".com",Phone).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+
+                                            DatabaseReference df=  Gaurds.child("Guards").child(mAuth.getCurrentUser().getUid());
+                                            df.child("Contact").setValue(Phone);
+                                            df.child("Name").setValue(Name).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+
+                                                    Toast.makeText(getApplicationContext(),"USER CREATED",Toast.LENGTH_SHORT).show();
+
+                                                }
+                                            });
+
+                                        }
+                                    });
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getApplicationContext(),"FAILED TO CREATE USER",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(),"Make sure fields are field and mobile number is valid 10 digit number",Toast.LENGTH_SHORT).show();
+                        }
+
+
+
+                    }
+                });
+
+
+            }
+        });
 
 
 
